21eca42fc02397ead1c5916ccc813e5fb8df8fc4
arthur-mdn <arthur@mondon.pro>
2023-03-09 20:13:00 +0100

    update config_sample.php to actual DB name

--- config_sample.php
+++ config_sample.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'username');
 define('DB_PASSWORD', 'password');
-define('DB_NAME', 'database_name');
+define('DB_NAME', 'chatbox_alpinejs');
 
 
 $conn2 = new PDO("mysql:dbname=" . DB_NAME . ";host=" . DB_SERVER, DB_USERNAME, DB_PASSWORD);
